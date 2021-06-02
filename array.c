105bab13cc016f5867473892ddc7d26a5f424892
Dharmendra Mehta <dharmendramehta622@gmail.com>
2021-06-02 19:46:18 +0545

    stack implementation using array

--- array.c
+++ array.c
@@ -1,20 +1,65 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct stack{
+struct stack
+{
     int size;
     int top;
     int *arr;
+};
+
+  int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == (ptr->size - 1))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
-int main(){
-struct stack s;
-s.size = 80;
-s.top = -1;
+int main()
+{
 
-s.arr = (int *) malloc(s.size * sizeof(int));
+    //struct as stack
+    // struct stack s;
+    // s.size = 80;
+    // s.top = -1;
+
+    // s.arr = (int *) malloc(s.size * sizeof(int));
+    
+    //Pushing an element manually
+    // s->arr[0] = 4;
+    // s->top++;
+
+    //struct as a pointer
+    struct stack *s;
+    s->size = 80;
+    s->top = -1;
+    s->arr = (int *)malloc(s->size * sizeof(int)),
+
+  
 
 
-return 0;
+    //check if stack is empty
+    if(isEmpty(s)){
+        printf(The stack is empty);
+    }else{
+        printf(The stack is not empty);
+    }
+    return 0;
 }
