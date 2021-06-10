cadf6213706bfd92e83240f2242dc3fd23846584
Dharmendra Mehta <dharmendramehta622@gmail.com>
2021-06-10 19:20:50 +0545

    dangling pointer

--- Pointer/null_pointer.c
+++ Pointer/null_pointer.c
@@ -14,10 +14,15 @@ int main()
     }
     else
     {
-        printf("Memory allocated sucessfully");
+        printf("Memory allocated sucessfully\n");
+
     }
     //endof useful for handling errors when using malloc function
-
+    printf("Memory Address is: \n");
+    printf("%d", ptr1);
+    printf("\n");
+    printf("Size of NULL is: \n");
+    printf("%d", sizeof(NULL));
     return 0;
 }
 
@@ -28,4 +33,13 @@ int main()
 //facts about NULL pointer
 ->the value of NULL is 0.We can either use NULL or 0 but this 0 is written in context of pointers and is not equivalent to the integer 0.
 
+-> size of NULL pointer depends upon the platform and is similar to the size of normal pointers.
+        printf('%d', ptr);
+
+*/
+
+/*Best Practices 
+-> it is good practice to initialize a pointer as NULL.
+-> it is good practice to perform NULL check before dereferencing any pointer to avoid surprises.
+->
 */
