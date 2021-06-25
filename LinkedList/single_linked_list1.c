a35299e3cdf916783d84d45c144e0d5f74b892f4
KodeMafia <dharmendramehta622@gmail.com>
2021-06-25 07:34:12 +0545

    accessing the nodes using single pointer

--- LinkedList/single_linked_list1.c
+++ LinkedList/single_linked_list1.c
@@ -16,9 +16,14 @@ int main()
     head->data = 45;
     head->link = NULL;
 
-    struct node *current = (struct node *)malloc(sizeof(struct node));
+    struct node *current = (struct node *)malloc(sizeof(struct node)); //!first part is for TYPECASTING (struct node *)
     current->data = 98;
     current->link = NULL;
     head->link = current;
+
+    current = malloc(sizeof(struct node));
+    current->data = 3;
+    current->link = NULL;
+    head->link->link = current;
     return 0;
 }
