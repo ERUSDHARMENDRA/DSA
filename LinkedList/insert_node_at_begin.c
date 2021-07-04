f4383e7ebedc491cdd1ae965d4d92b05b6b9c67e
KodeMafia <dharmendramehta622@gmail.com>
2021-07-04 21:35:27 +0545

    insert node at begin

--- LinkedList/insert_node_at_begin.c
+++ LinkedList/insert_node_at_begin.c
@@ -5,7 +5,7 @@
 struct node {
     int data;
     struct node *link;
-}
+};
 
 struct node* add_beg(struct node* head, int d){
     struct node *ptr = malloc(sizeof(struct node));
@@ -33,7 +33,7 @@ int main(){
     ptr = head;
     while (ptr!=NULL)
     {
-        printf("%d", ptr->data);
+        printf("\n%d", ptr->data);
         ptr = ptr->link;
     }
     
