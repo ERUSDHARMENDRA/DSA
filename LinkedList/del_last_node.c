18fede3ba65b3d7fffed126811c0772f0dd32073
KodeMafia <dharmendramehta622@gmail.com>
2021-07-12 20:57:52 +0545

    chapter 50

---
+++ LinkedList/del_last_node.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node *link;
+};
+
+struct node *del_last(struct node *head){
+    if(head==NULL){
+        printf("List is already empty !");
+    }else if(head->link==NULL){
+        free(head);
+        head = NULL;
+    }else {
+        struct node *temp = head;
+        struct node *temp2 = head;
+        while (temp->link !=NULL)
+        {
+            temp2 = temp;
+            temp = temp->link;
+        }
+        temp2->link = NULL;
+        free(temp);
+    }
+    return head;
+}
+
+int main(){
+    struct node *head = malloc(sizeof(struct node));
+    struct node *ptr = head;
+    ptr = head;
+
+    while (ptr!=NULL)
+    {
+        printf("%d", ptr->data);
+        ptr = ptr->link;
+    }
+
+    head = del_last(head);
+    return 0;
+}
---
+++ LinkedList/delete_first_node.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+struct node *del_first(struct node *head)
+{
+    if (head == NULL)
+    {
+        printf("List is already empty !");
+    }
+    else
+    {
+        struct node *temp = head;
+        head = head->link;
+        free(temp);
+    }
+    return head;
+}
+
+int main()
+{
+    struct node *head = malloc(sizeof(struct node));
+    struct node *ptr = head;
+    head = del_first(head);
+    ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d", ptr->data);
+        ptr = ptr->link;
+    }
+
+    return 0;
+}
