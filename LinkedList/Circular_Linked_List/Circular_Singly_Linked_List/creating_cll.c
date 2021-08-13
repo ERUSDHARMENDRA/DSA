e880f1890289addd8853d74ef300bf17969a2e1c
kode-mafia <dharmendramehta622@gmail.com>
2021-08-13 07:41:18 +0545

    cicularly linked list chapter 2 completed

---
+++ LinkedList/Circular_Linked_List/Circular_Singly_Linked_List/creating_cll.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+int data;
+struct node* next;
+};
+
+struct node* circularSingly(int data){
+    struct node* temp = malloc(sizeof(struct node));
+    temp->data = data;
+    temp->next = temp;
+    return temp;
+}
+
+struct node* circularDoubly(int data){
+    struct node* temp = malloc(sizeof(struct node));
+    temp->data = data;
+    temp->next = temp;
+    return temp;
+}
+
+
+
+int main(){
+    int dataS =34;
+    int dataD=45;
+    struct node* tailS;
+    struct node* tailD;
+    tailS = circularSingly(dataS);
+    tailD = circularDoubly(dataD);
+
+    printf("%d\n",tailS->data);
+    printf("%d\n", tailD->data);
+    
+    return 0;
+}
